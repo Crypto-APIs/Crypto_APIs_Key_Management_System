2ff768fc360f2f09e5e13afe2d1b5fa2cce2c978
Kristiyan Ivanov <kristiyan.ivanov@menasoftware.com>
2022-08-29 12:37:25 +0300

    v0.1.5 - improve readme.md and remove redundant functionality

--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,6 @@
 const HDWalletService = require('./hdWalletsService')
     , WalletService = require('./walletService')
     , BroadcastService = require('./broadcastService')
-    , CallbacksService = require('./callbacksService')
     , SubscriptionsService = require('./subscriptionsService')
     , PrepareService = require('./prepareService')
     , SignService = require('./signService')
@@ -13,7 +12,6 @@ module.exports = {
     HDWalletService,
     WalletService,
     BroadcastService,
-    CallbacksService,
     SubscriptionsService,
     PrepareService,
     SignService
